ab916021ecfc930124521b934bd6b16eca809571
Zhandos Duisen <duisenzhandos@gmail.com>
2024-07-29 17:52:34 +0500

    add migration for db

--- app/tooling/sales-admin/main.go
+++ app/tooling/sales-admin/main.go
@@ -10,6 +10,9 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"github.com/Zhandos28/ardanlabs-service/business/data/dbmigrate"
+	db "github.com/Zhandos28/ardanlabs-service/business/data/dbsql/pgx"
+	"github.com/ardanlabs/conf/v3"
 	"io"
 	"log"
 	"os"
@@ -26,11 +29,70 @@ var (
 )
 
 func main() {
-	err := gentoken()
+	err := migrateSeed()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func migrateSeed() error {
+	var cfg struct {
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:database-service.sales-system.svc.cluster.local"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:2"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
+	}
+
+	const prefix = "SALES"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	dbConfig := db.Config{
+		User:         cfg.DB.User,
+		Password:     cfg.DB.Password,
+		Host:         cfg.DB.Host,
+		Name:         cfg.DB.Name,
+		MaxIdleConns: cfg.DB.MaxIdleConns,
+		MaxOpenConns: cfg.DB.MaxOpenConns,
+		DisableTLS:   cfg.DB.DisableTLS,
+	}
+
+	db, err := db.Open(dbConfig)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := dbmigrate.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+
+	// -------------------------------------------------------------------------
+
+	if err := dbmigrate.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
 }
 
 func gentoken() error {
